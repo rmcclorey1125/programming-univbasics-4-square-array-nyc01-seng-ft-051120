d667b9a22cc780f40e09fda1c624306e0d31daea
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-24 20:05:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(numbers)
   new_array = []
-  numbers.length.times do |num|
-    new_array << (num ** 2)
+  numbers.length.times do |index|
+    new_array << (array|index| ** 2)
 end
 return new_array
 end
